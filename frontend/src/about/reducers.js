b642b48682affe19b13e7b2400f7329bb449d169
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-06 15:15:29 -0300

    Reducers config

---
+++ frontend/src/about/reducers.js
@@ -0,0 +1,12 @@
+import {
+    combineReducers
+} from 'redux'
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: '',
+        list: [{}]
+    })
+})
+
+export default rootReducer
