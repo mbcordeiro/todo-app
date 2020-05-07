966829c278f783fd23d979655000bd617430eeba
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-07 11:28:39 -0300

    Clear form

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
   constructor(props) {
@@ -17,7 +17,7 @@ class TodoForm extends Component {
   }
   
   keyHandler(e) {
-    const { add, search, description } = this.props
+    const { add, search, description, clear } = this.props
     if (e.key === 'Enter') {
         e.shiftKey ? search() : add(description)
     } else if (e.key === 'Escape') {
@@ -53,7 +53,7 @@ class TodoForm extends Component {
           <IconButton
             style="default"
             icon="close"
-            onClick={this.props.handleClear}
+            onClick={this.props.clear}
           ></IconButton>
         </Grid>
       </div>
